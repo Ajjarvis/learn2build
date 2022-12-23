a4091838ce8f7999e4016072553efca5d3263627
Ajay Chaudhari <archaudhari1402@gmail.com>
2022-12-24 00:15:57 +0530

    [Sort] Added Insertion Sort

---
+++ algos/sorting/Insertion_sort.py
@@ -0,0 +1,17 @@
+def swap(i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+arr =  [12, 11, 13, 5, 6]
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
+print("Sorted Array is : ", insertion_sort(arr))
